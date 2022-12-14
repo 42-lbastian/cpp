803c9f81c9fa7f22a6d0b6cbb55d0e7427cf35f1
Loic BASTIAN <lbastian@e1r1p8.clusters.42paris.fr>
2022-12-14 16:23:30 +0100

    Ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string& stringREF = str;
+
+	std::cout << "Memory address of str:\t\t" << &str << std::endl;
+	std::cout << "Memory address of stringPTR:\t" << stringPTR << std::endl;
+	std::cout << "Memory address of stringREF:\t" << &stringREF << std::endl;
+
+	std::cout << "Value of str:\t\t" << str << std::endl; 
+	std::cout << "Value of stringPTR:\t" << *stringPTR << std::endl; 
+	std::cout << "Value of stringREF:\t" << stringREF << std::endl; 
+
+	return (0);
+}
