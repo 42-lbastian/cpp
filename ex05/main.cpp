cb0a857a3f4ff36fe25364ba0ed30dbbc866ceed
Loic BASTIAN <lbastian@e1r7p13.clusters.42paris.fr>
2022-12-27 18:06:57 +0100

    Add test main

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -3,6 +3,19 @@
 int main(void)
 {
 	Harl harl = Harl();
-	harl.complain("toto");
+	
+	std::cout << "Incorrect Level" << std::endl;
+	harl.complain("IncorrectLevel");
+
+	std::cout << std::endl << "Error" << std::endl;
 	harl.complain("error");
+
+	std::cout << std::endl << "Debug" << std::endl;
+	harl.complain("debug");
+
+	std::cout << std::endl << "Info" << std::endl;
+	harl.complain("info");
+
+	std::cout << std::endl << "Warning" << std::endl;
+	harl.complain("warning");
 }
