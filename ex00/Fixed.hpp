3452bd5f4451fe3e3b02a1170f998c46091ce5a6
Loic BASTIAN <lbastian@e1r2p15.clusters.42paris.fr>
2022-12-29 20:45:00 +0100

    Ex00

--- ex00/Fixed.hpp
+++ ex00/Fixed.hpp
@@ -1,15 +1,21 @@
 #ifndef FIXED_HPP
 #define FIXED_HPP
 
+#include <iostream>
+
 class Fixed
 {
 	public:
 		Fixed(void);
 		~Fixed(void);
 		Fixed(const Fixed &fixed);
-	
+		Fixed &operator=(const Fixed &fixed);
+		int  getRawBits(void) const;
+		void setRawBits(int const raw);
 
-
+	private:
+		int _fixed_point;
+		static const int _nb_fract_bits = 8;
 };
 
 
