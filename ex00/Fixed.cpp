d20f4ae0b626d589b18cf363c627eee8e4427a0c
Loic BASTIAN <lbastian@e1r2p15.clusters.42paris.fr>
2022-12-29 17:22:49 +0100

    Start canonical form

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,17 @@
+#include "Fixed.hpp"
+
+
+Fixed::Fixed(void)
+{
+
+}
+
+Fixed::~Fixed(void)
+{
+
+}
+
+Fixed::Fixed(const Fixed &fixed)
+{
+	*this = fixed;
+}
---
+++ ex00/Fixed.hpp
@@ -0,0 +1,16 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+class Fixed
+{
+	public:
+		Fixed(void);
+		~Fixed(void);
+		Fixed(const Fixed &fixed);
+	
+
+
+};
+
+
+#endif
