0a11d8f0993c6e01601999c30a578bed92129956
Loic BASTIAN <lbastian@e1r2p13.clusters.42paris.fr>
2023-01-27 17:10:19 +0100

    Add previous file

---
+++ cpp05/ex02/main.cpp
@@ -0,0 +1,48 @@
+#include "Bureaucrat.hpp"
+#include "Form.hpp"
+
+int main(void)
+{
+	Bureaucrat toto(3, "Toto");
+	Form form(2, 1, "Formulaire");
+	std::cout << form << std::endl;
+	form.beSigned(toto);
+	std::cout << form << std::endl;
+	toto.incrGrade();
+	form.beSigned(toto);
+	std::cout << form << std::endl;
+	std::cout << std::endl;
+
+	try
+	{
+		Form form2(0, 1, "Formulaire");
+	}
+	catch (Form::GradeTooHighException& high)
+	{
+		std::cout << high.what() << std::endl;
+	}
+	try
+	{
+		Form form2(1, 0, "Formulaire");
+	}
+	catch (Form::GradeTooHighException& high)
+	{
+		std::cout << high.what() << std::endl;
+	}
+	try
+	{
+		Form form2(151, 1, "Formulaire");
+	}
+	catch (Form::GradeTooLowException& low)
+	{
+		std::cout << low.what() << std::endl;
+	}
+	try
+	{
+		Form form2(15, 151, "Formulaire");
+	}
+	catch (Form::GradeTooLowException& low)
+	{
+		std::cout << low.what() << std::endl;
+	}
+}
