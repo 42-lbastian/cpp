23c5ced9dacb26a5197c2e138fa538d0b639fdd4
Loic BASTIAN <lbastian@e1r4p10.clusters.42paris.fr>
2023-02-24 13:50:46 +0100

    Finish ex01

--- cpp07/ex01/iter.hpp
+++ cpp07/ex01/iter.hpp
@@ -3,8 +3,8 @@
 
 #include <iostream>
 
-template<typename T, typename U, typename V>
-void iter(T& array, U& size, V& func)
+template<typename T, typename U>
+void iter(T* array, U size, void (*func)(T&))
 {
 	for (int i = 0; i < size; i++)
 		func(array[i]);
@@ -13,7 +13,13 @@ void iter(T& array, U& size, V& func)
 template<typename T>
 void print(T& to_print)
 {
-	std::cout << to_print << std::endl;
+	std::cout << to_print << " ";
+}
+
+template<typename T>
+void add_one(T& to_add)
+{
+	to_add++;
 }
 
 #endif
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -1,9 +1,32 @@
 #include "iter.hpp"
+#define NB_ELEM 4 
+
+void add_poney(std::string& str)
+{
+	str += "_poney";
+}
 
 int main(void)
 {
-	char array_c[4] = {'a', 'b', 'c', 'd'};
+	char array_c[NB_ELEM] = {'a', 'b', 'c', 'd'};
+	int array_i[NB_ELEM] = {0, 1, 2, 3};
+	std::string array_s[NB_ELEM] = {"cat", "dog", "fish", "poupoule"};
 
-	iter(array_c, 4, &print);
+	iter(array_c, NB_ELEM, &print);
+	std::cout << std::endl;
+	iter(array_c, NB_ELEM, &add_one);
+	iter(array_c, NB_ELEM, &print);
+	std::cout << std::endl;
+
+	iter(array_i, NB_ELEM, &print);
+	std::cout << std::endl;
+	iter(array_i, NB_ELEM, &add_one);
+	iter(array_i, NB_ELEM, &print);
+	std::cout << std::endl;
+
+	iter(array_s, NB_ELEM, &print);
+	std::cout << std::endl;
+	iter(array_s, NB_ELEM, &add_poney);
+	iter(array_s, NB_ELEM, &print);
 	return (0);
 }
