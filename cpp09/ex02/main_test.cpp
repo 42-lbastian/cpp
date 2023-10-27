6d725b0498b996c92263feb193731d7635164155
lbastian <loic.bastian@gmail.com>
2023-10-27 16:10:26 +0200

    Start sort vect

---
+++ cpp09/ex02/main_test.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <vector>
+#include <limits.h>
+#include <stdlib.h>
+
+int ft_atoi(const std::string str, int& nb)
+{
+	int i;
+	long test;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (1);
+		i++;
+	}
+	test = atol(str.c_str());
+	if (i > 9 && test > INT_MAX)
+		return (2);
+	nb = atoi(str.c_str());
+	return (0);
+}
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int ft_create_vect(std::vector<int>& vect, char** argv, int argc)
+{
+	int nb;
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (ft_strlen(argv[i]) == 0 || ft_atoi(argv[i], nb))
+			return (1);
+		else
+			vect.push_back(nb);
+	}
+	return (0);
+}
+
+void ft_print_vect(std::vector<std::vector<int> > vect)
+{
+	for (int i = 0; i < (int)vect.size(); i++)
+	{
+		for (int j = 0; j < (int)vect[i].size(); j++)
+			std::cout << vect[i][j] << " ";
+		std::cout << std::endl;
+	}
+}
+
+void ft_sort_pair(std::vector<std::vector<int> >& vect_pair)
+{
+	int temp;
+
+	for (int i = 0; i < (int)vect_pair.size(); i++)
+	{
+		if (vect_pair[i][0] < vect_pair[i][1])
+		{
+			temp = vect_pair[i][1];
+			vect_pair[i][1] = vect_pair[i][0];
+			vect_pair[i][0] = temp;
+		}
+	}
+}
+
+void ft_create_pair(const std::vector<int>& vect, std::vector<std::vector<int> >& vect_pair)
+{
+	std::vector<int> vect_temp;
+	for (int i = 0; i < (int)vect.size(); i++)
+	{
+		vect_temp.push_back(vect[i]);
+		i++;
+		vect_temp.push_back(vect[i]);
+		vect_pair.push_back(vect_temp);
+		vect_temp.pop_back();
+		vect_temp.pop_back();
+	}
+}
+
+void ft_mi_sort(std::vector<int>& vect)
+{
+	bool odd_even;
+	int save;
+	std::vector<std::vector<int> > vect_pair;
+
+	save = -1;
+	odd_even = vect.size() % 2; // true == odd / false == even
+	if (odd_even == true)
+	{
+		save = vect.back();
+		vect.pop_back();
+	}
+	ft_create_pair(vect, vect_pair);
+		ft_print_vect(vect_pair);
+	std::cout << "------------" << std::endl;
+	ft_sort_pair(vect_pair);
+		ft_print_vect(vect_pair);
+	std::cout << save << std::endl;
+}
+
+
+
+///////////////////////////////////CHECK VALEUR UNIQUE
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+	{
+		std::vector<int> vect;
+		if (ft_create_vect(vect, argv, argc) == 0)
+		{
+			ft_mi_sort(vect);
+		}
+		else
+		{
+			std::cout << "Invalid number in argument" << std::endl;
+			return (1);
+		}
+	}
+	else
+		std::cout << "No argument" << std::endl;
+	return (0);
+}
